5446df45eb2d691822b1656784d4857ce539bffa
Abdelmageed Sayed <abdelmageedsayed@hotmail.com>
2017-02-28 21:05:22 +0200

    lesson 2 - (Components)

---
+++ simple-todos/client/imports/ui/App.js
@@ -0,0 +1,74 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _TaskJsx = require('./Task.jsx');
+
+var _TaskJsx2 = _interopRequireDefault(_TaskJsx);
+
+var App = (function (_Component) {
+  _inherits(App, _Component);
+
+  function App() {
+    _classCallCheck(this, App);
+
+    _get(Object.getPrototypeOf(App.prototype), 'constructor', this).apply(this, arguments);
+  }
+
+  _createClass(App, [{
+    key: 'getTasks',
+    value: function getTasks() {
+      return [{ _id: 1, text: 'This is task 1' }, { _id: 2, text: 'This is task 2' }, { _id: 3, text: 'This is task 3' }];
+    }
+  }, {
+    key: 'renderTasks',
+    value: function renderTasks() {
+      return this.getTasks().map(function (task) {
+        return _react2['default'].createElement(_TaskJsx2['default'], { key: task._id, task: task });
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement(
+        'div',
+        { className: 'container' },
+        _react2['default'].createElement(
+          'header',
+          null,
+          _react2['default'].createElement(
+            'h1',
+            null,
+            'Todo List'
+          ),
+          _react2['default'].createElement(
+            'ul',
+            null,
+            this.renderTasks()
+          )
+        )
+      );
+    }
+  }]);
+
+  return App;
+})(_react.Component);
+
+exports['default'] = App;
+module.exports = exports['default'];
---
+++ simple-todos/client/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _meteorMeteor = require('meteor/meteor');
+
+var _reactDom = require('react-dom');
+
+var _importsUiAppJsx = require('./imports/ui/App.jsx');
+
+var _importsUiAppJsx2 = _interopRequireDefault(_importsUiAppJsx);
+
+_meteorMeteor.Meteor.startup(function () {
+  (0, _reactDom.render)(_react2['default'].createElement(_importsUiAppJsx2['default'], null), document.getElementById('render-target'));
+});
